728aaa80b18538765357a0e9d7bbef0ea576651c
洪华俊 <erfu81856@gmail.com>
2025-03-22 19:00:04 +0800

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -15,7 +13,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let capitalized = first.to_uppercase().to_string();
+            let rest = c.collect::<String>();
+            capitalized + &rest
+        }
     }
 }
 
@@ -24,7 +26,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    vec![capitalize_first(words[0]), capitalize_first(words[1])]
 }
 
 // Step 3.
@@ -32,7 +34,10 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words
+        .iter()
+        .map(|word| capitalize_first(word))
+        .collect::<String>()
 }
 
 #[cfg(test)]
--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,13 +6,11 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> String {
     if x.len() > y.len() {
-        x
+        x.to_string()
     } else {
-        y
+        y.to_string()
     }
 }
 
@@ -22,6 +20,9 @@ fn main() {
     {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
+        //在 main 函数中，string2 是一个局部变量，其生命周期仅限于内部的块作用域 {}。
+        //调用 longest(string1.as_str(), string2.as_str()) 时，longest 函数的返回值的生命周期被绑定到两个输入参数中较短的那个生命周期（即 string2 的生命周期）。
+        //当离开内部块作用域后，string2 被销毁，因此 result 引用了一个已经失效的字符串，导致编译错误。
     }
     println!("The longest string is '{}'", result);
 }
