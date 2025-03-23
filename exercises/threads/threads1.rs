6e84063f74243fa7989146e8895d969776221344
洪华俊 <erfu81856@gmail.com>
2025-03-23 16:32:33 +0800

    update

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,8 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -26,6 +24,9 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
+        if let Ok(elapsed_time) = handle.join() {
+            results.push(elapsed_time)
+        }
         // TODO: a struct is returned from thread::spawn, can you use it?
     }
 
