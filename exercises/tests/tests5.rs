64503aa1ee3bb9eaca191ac5caa8c5724f062d01
honghuajun <erfu81856@gmail.com>
2025-03-24 11:04:23 +0800

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,7 +22,6 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 /// # Safety
 ///
@@ -31,8 +30,12 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+
+    // SAFETY: The caller guarantees that `address` is a valid pointer to a
+    // mutable `u32` value. We dereference the pointer and modify its value.
     unsafe {
-        todo!("Your code goes here")
+        let ptr = address as *mut u32;
+        *ptr = 0xAABBCCDD; 
     }
 }
 
