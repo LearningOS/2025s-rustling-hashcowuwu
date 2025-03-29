4d1f4424bf45771c48112b111574222800062f2d
洪华俊 <erfu81856@gmail.com>
2025-03-29 20:22:40 +0800

    update

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,11 +1,12 @@
 /*
-	heap
-	This question requires you to implement a binary heap function
+    heap
+    This question requires you to implement a binary heap function
 */
 // I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
+// use std::f32::consts::PI;
 
 pub struct Heap<T>
 where
@@ -37,7 +38,24 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.count += 1;
+        if self.items.len() <= self.count {
+            self.items.push(value);
+        } else {
+            self.items[self.count] = value;
+        }
+        self.swim(self.count);
+    }
+
+    fn swim(&mut self, mut idx: usize) {
+        while idx > 1 {
+            let parent_idx = self.parent_idx(idx); // 提前计算父节点索引
+            if !(self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                break;
+            }
+            self.items.swap(idx, parent_idx); // 使用局部变量，避免冲突
+            idx = parent_idx;
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +75,37 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+
+        if right <= self.count && (self.comparator)(&self.items[right], &self.items[left]) {
+            right
+        } else {
+            left
+        }
+    }
+
+    fn next(&mut self) -> Option<T> {
         //TODO
-		0
+        if self.count == 0 {
+            return None;
+        }
+
+        let result = Some(std::mem::replace(&mut self.items[1], T::default()));
+        self.items.swap(1, self.count);
+        self.sink(1);
+        result
+    }
+
+    fn sink(&mut self, mut idx: usize) {
+        while self.children_present(idx) {
+            let child_idx = self.smallest_child_idx(idx);
+            if !(self.comparator)(&self.items[child_idx], &self.items[idx]) {
+                break;
+            }
+            self.items.swap(idx, child_idx);
+            idx = child_idx;
+        }
     }
 }
 
@@ -82,10 +129,8 @@ where
     T: Default,
 {
     type Item = T;
-
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        self.next()
     }
 }
 
